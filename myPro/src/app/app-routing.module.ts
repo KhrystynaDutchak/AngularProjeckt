58c942a33760d277b3fd111b796fe6ff5180e5f2
ваше имяKristina <you@example.com>
2020-11-15 18:07:21 +0200

    Story 182: Fix benchmarks for speed

--- myPro/src/app/app-routing.module.ts
+++ myPro/src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./Components/home/home.component";
+import {EducationComponent} from "./Components/education/education.component";
+import {ProjecktComponent} from "./Components/projeckt/projeckt.component";
+import {SkillsComponent} from "./Components/skills/skills.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'education',
+    component: EducationComponent
+  },
+  {
+    path: 'project',
+    component: ProjecktComponent
+  },
+  {
+    path: 'skills',
+    component: SkillsComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- myPro/src/app/app.module.ts
+++ myPro/src/app/app.module.ts
@@ -4,10 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
-import { ContactsComponent } from './Components/contacts/contacts.component';
-import { AboutMeComponent } from './Components/about-me/about-me.component';
 import { EducationComponent } from './Components/education/education.component';
-import { InfoComponent } from './Components/info/info.component';
 import { SkillsComponent } from './Components/skills/skills.component';
 import { ProjecktComponent } from './Components/projeckt/projeckt.component';
 
@@ -15,10 +12,7 @@ import { ProjecktComponent } from './Components/projeckt/projeckt.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    ContactsComponent,
-    AboutMeComponent,
     EducationComponent,
-    InfoComponent,
     SkillsComponent,
     ProjecktComponent
   ],
